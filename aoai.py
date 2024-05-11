3fa5d48198a2172266d35e668ca79130220b5cc4
Simon Kurtz <simonkurtz@microsoft.com>
2024-05-11 12:56:59 -0400

    Refactor

--- aoai.py
+++ aoai.py
@@ -1,7 +1,8 @@
 from azure.identity import DefaultAzureCredential # type: ignore
 from datetime import datetime
 from openai import AzureOpenAI, DefaultHttpxClient, NotFoundError
-from load_balancer import LoadBalancer, Backends
+from load_balancer import LoadBalancer, Backend
+from typing import List
 import time
 import traceback
 
@@ -11,20 +12,20 @@ def token_provider():
     return token.token
 
 # Standard Azure OpenAI Implementation (One Backend)
-def send_request(numOfRequests):
+def send_request(num_of_requests, azure_endpoint: str):
     try:
         client = AzureOpenAI(
-            azure_endpoint="https://<your-aoai-instance>.openai.azure.com",
-            azure_ad_token_provider=token_provider,
-            api_version="2024-04-01-preview"
+            azure_endpoint = azure_endpoint,
+            azure_ad_token_provider = token_provider,
+            api_version = "2024-04-01-preview"
         )
 
-        for i in range(numOfRequests):
-            print(f"{datetime.now()}: Standard request {i+1}/{numOfRequests}")
+        for i in range(num_of_requests):
+            print(f"{datetime.now()}: Standard request {i+1}/{num_of_requests}")
         
             response = client.chat.completions.create(
-                model="<your-aoai-model>",
-                messages=[
+                model = MODEL,
+                messages = [
                     {"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"}
                 ]
@@ -40,31 +41,24 @@ def send_request(numOfRequests):
         traceback.print_exc()
 
 # Load-balanced Azure OpenAI Implementation (Multiple Backends)
-def send_loadbalancer_request(numOfRequests):
+def send_loadbalancer_request(num_of_requests, backends: Backend):
     try:
-        # Define the backends and their priority
-        backends = [
-            Backends("<your-aoai-instance-1>.openai.azure.com", 1),
-            Backends("<your-aoai-instance-2>.openai.azure.com", 1),
-            Backends("<your-aoai-instance-3>.openai.azure.com", 1)
-        ]
-
         # Instantiate the LoadBalancer class and create a new https client with the LoadBalancer as the injected transport
         lb = LoadBalancer(backends)        
 
         client = AzureOpenAI(
-            azure_endpoint=f"https://{backends[0].host}",   # Must be seeded, so we use the first host. It will get overwritten by the LoadBalancer
-            azure_ad_token_provider=token_provider,
-            api_version="2024-04-01-preview",
-            http_client = DefaultHttpxClient(transport=lb)  # Inject the LoadBalancer as the transport in a new default httpx client
+            azure_endpoint = f"https://{backends[0].host}",   # Must be seeded, so we use the first host. It will get overwritten by the LoadBalancer
+            azure_ad_token_provider = token_provider,
+            api_version = "2024-04-01-preview",
+            http_client = DefaultHttpxClient(transport=lb)    # Inject the LoadBalancer as the transport in a new default httpx client
         )
 
-        for i in range(numOfRequests):
-            print(f"{datetime.now()}: LoadBalancer request {i+1}/{numOfRequests}")
+        for i in range(num_of_requests):
+            print(f"{datetime.now()}: LoadBalancer request {i+1}/{num_of_requests}")
         
             response = client.chat.completions.create(
-                model="<your-aoai-model>",
-                messages=[
+                model = MODEL,
+                messages = [
                     {"role": "system", "content": "You are a helpful assistant."},
                     {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"}
                 ]
@@ -84,20 +78,36 @@ def send_loadbalancer_request(numOfRequests):
 
 #############################################################################
 
-num_of_requests = 5
+# >>> Only make changes to MODEL, AZURE_ENDPOINT, and the backends list <<<
+
+
+
+# Common Constants
+NUM_OF_REQUESTS = 5
+MODEL = "<your-aoai-model>"  # the model is common across standard and load-balanced requests
 
 # Standard requests to one AOAI backend
+AZURE_ENDPOINT = "https://oai-eastus-xxxxxxxx.openai.azure.com"
 start_time = time.time()
-send_request(num_of_requests)
+
+send_request(NUM_OF_REQUESTS, AZURE_ENDPOINT)
 end_time = time.time()
 
 # Load-balanced requests to one or more AOAI backends
+# Define the backends and their priority
+backends: List[Backend] = [
+    Backend("oai-eastus-xxxxxxxx.openai.azure.com", 1),
+    Backend("oai-southcentralus-xxxxxxxx.openai.azure.com", 1),
+    Backend("oai-westus-xxxxxxxx.openai.azure.com", 1)
+]
 lb_start_time = time.time()
-send_loadbalancer_request(num_of_requests)
+send_loadbalancer_request(NUM_OF_REQUESTS, backends)
 lb_end_time = time.time()
 
+# Statistics
+
 print("\n")
-print(f"Number of requests                 : {num_of_requests}")
+print(f"Number of requests                 : {NUM_OF_REQUESTS}")
 print(f"Single instance operation duration : {end_time - start_time:.2f} seconds")
 print(f"Load-balancer operation duration   : {lb_end_time - lb_start_time:.2f} seconds")
 print("\n\n")
--- load_balancer.py
+++ load_balancer.py
@@ -1,9 +1,10 @@
 from datetime import datetime, MINYEAR, MAXYEAR, timedelta, timezone
 from dateutil.tz import tzutc
 from httpx import Client, Response, BaseTransport
+from typing import List
 import traceback
 
-class Backends:
+class Backend:
     def __init__(self, host: str, priority: int):
         self.host = host
         self.priority = priority
@@ -17,54 +18,54 @@ class LoadBalancer(BaseTransport):
             self.reset_statistics()
 
         def reset_statistics(self):
-            self.TotalRequests = 0     
-            self.TotalFailures = 0
-            self.TotalSuccesses = 0      
-            self.BackendStats = {} 
+            self.total_requests = 0     
+            self.total_failures = 0
+            self.total_successes = 0      
+            self.backends_stats = {} 
 
         def update_backend_stats(self, host, success):
-            if host not in self.BackendStats:
-                self.BackendStats[host] = {'attempts': 0, 'successes': 0, 'failures': 0}
+            if host not in self.backends_stats:
+                self.backends_stats[host] = {'attempts': 0, 'successes': 0, 'failures': 0}
             if success:
-                self.BackendStats[host]['attempts'] += 1
-                self.BackendStats[host]['successes'] += 1
-                self.TotalSuccesses += 1
+                self.backends_stats[host]['attempts'] += 1
+                self.backends_stats[host]['successes'] += 1
+                self.total_successes += 1
             else:
-                self.BackendStats[host]['attempts'] += 1
-                self.BackendStats[host]['failures'] += 1
-                self.TotalFailures += 1
+                self.backends_stats[host]['attempts'] += 1
+                self.backends_stats[host]['failures'] += 1
+                self.total_failures += 1
 
-            self.TotalRequests += 1
+            self.total_requests += 1
 
         def print(self):
-            longest_host_string = len(max(self.BackendStats.keys(), key=len))
+            longest_host_string = len(max(self.backends_stats.keys(), key=len))
             repeater = longest_host_string + 48
             
             print("*" * repeater)
-            pad = len(str(self.TotalRequests))    
-            print(f"\nLoad Balancer Statistics: \n\nTotal Requests  : {str(self.TotalRequests).rjust(pad)}\nTotal Successes : {str(self.TotalSuccesses).rjust(pad)}\nTotal Failures  : {str(self.TotalFailures).rjust(pad)}\n")
+            pad = len(str(self.total_requests))    
+            print(f"\nLoad Balancer Statistics: \n\nTotal Requests  : {str(self.total_requests).rjust(pad)}\nTotal Successes : {str(self.total_successes).rjust(pad)}\nTotal Failures  : {str(self.total_failures).rjust(pad)}\n")
 
             # Print the statistics per backend
             print(f"{'Backend':<{longest_host_string + 2}} {'Distribution %':<15} {'Attempts':<9} {'Successes':<10} {'Failures':<10}")
             print("-" * repeater)
-            for host, stats in self.BackendStats.items():
-                print(f"{host:<{longest_host_string + 2}} {round((stats['attempts'] * 100 / self.TotalRequests), 2):>14} {stats['attempts']:>9} {stats['successes']:>10} {stats['failures']:>9}")
+            for host, stats in self.backends_stats.items():
+                print(f"{host:<{longest_host_string + 2}} {round((stats['attempts'] * 100 / self.total_requests), 2):>14} {stats['attempts']:>9} {stats['successes']:>10} {stats['failures']:>9}")
             print("\n")
             print("*" * repeater)
     
     # Constructor
-    def __init__(self, backends: 'LoadBalancer.Backends' = []):
+    def __init__(self, backends: List[Backend]):
         self._transport = Client()
         self.statistics = self.Statistics()
-        self.Backends = backends
+        self.backends = backends
         self._backendIndex = -1
-        self._remainingBackends = 1        
+        self._remainingBackend = 1        
 
     # Private Methods
     def _check_throttling(self):
         min_datetime = datetime(MINYEAR, 1, 1, tzinfo=tzutc())
 
-        for backend in self.Backends:
+        for backend in self.backends:
             if backend.is_throttling and datetime.now(tzutc()) >= backend.retry_after:
                 backend.is_throttling = False
                 backend.retry_after = min_datetime
@@ -72,53 +73,53 @@ class LoadBalancer(BaseTransport):
     
     def _get_backendIndex(self):
         # This is the main logic to pick the backend to be used
-        selectedPriority = float('inf')
-        availableBackends = []
+        selected_priority = float('inf')
+        available_Backend = []
 
-        for i in range(len(self.Backends)):
-            backend = self.Backends[i]
+        for i in range(len(self.backends)):
+            backend = self.backends[i]
 
             if not backend.is_throttling:
                 backendPriority = backend.priority
 
-                if backendPriority < selectedPriority:
-                    selectedPriority = backendPriority
-                    availableBackends.clear()
-                    availableBackends.append(i)
-                elif backendPriority == selectedPriority:
-                    availableBackends.append(i)
+                if backendPriority < selected_priority:
+                    selected_priority = backendPriority
+                    available_Backend.clear()
+                    available_Backend.append(i)
+                elif backendPriority == selected_priority:
+                    available_Backend.append(i)
 
-        if len(availableBackends) == 1:
-            return availableBackends[0]
+        if len(available_Backend) == 1:
+            return available_Backend[0]
 
-        if len(availableBackends) > 0:
+        if len(available_Backend) > 0:
             # Select the backend with the lowest successful_call_count to achieve a more balanced distribution than what random.choice() could provide over a variable length array
-            min_successful_call_count = self.Backends[availableBackends[0]].successful_call_count
-            selected_backend = availableBackends[0]
+            min_successful_call_count = self.backends[available_Backend[0]].successful_call_count
+            selected_backend = available_Backend[0]
 
-            for i in availableBackends[1:]:
-                if self.Backends[i].successful_call_count < min_successful_call_count:
-                    min_successful_call_count = self.Backends[i].successful_call_count
+            for i in available_Backend[1:]:
+                if self.backends[i].successful_call_count < min_successful_call_count:
+                    min_successful_call_count = self.backends[i].successful_call_count
                     selected_backend = i
 
             return selected_backend            
         else:
-            # If there are no available backends, -1 will be returned to indicate that nothing is available (and that we should bail).
+            # If there are no available Backend, -1 will be returned to indicate that nothing is available (and that we should bail).
             return -1
 
-    def _get_remainingBackends(self):
-        self._remainingBackends = 0
+    def _get_remainingBackend(self):
+        self._remainingBackend = 0
 
-        for backend in self.Backends:
+        for backend in self.backends:
             if not backend.is_throttling:
-                self._remainingBackends += 1
+                self._remainingBackend += 1
 
-        return self._remainingBackends
+        return self._remainingBackend
 
     def _get_soonest_retry_after(self):
         soonest_retry_after = datetime(MAXYEAR, 1, 1, tzinfo=tzutc())
         
-        for backend in self.Backends:
+        for backend in self.backends:
             if backend.is_throttling and backend.retry_after < soonest_retry_after:
                 soonest_retry_after = backend.retry_after
                 soonest_backend = backend.host
@@ -127,25 +128,28 @@ class LoadBalancer(BaseTransport):
         print(f"{datetime.now()}:   Soonest Retry After: {soonest_backend} - {str(delay)} second(s)")
 
         return delay
+    
+    def _return_429(self):
+        return self._return_429()
 
     # Public Methods
     def handle_request(self, request):
         self._check_throttling()
-        self._get_remainingBackends()
+        self._get_remainingBackend()
         response = None
         
-        while True and self._remainingBackends > 0:            
+        while True and self._remainingBackend > 0:            
             backendIndex = self._get_backendIndex()
             
             if backendIndex == -1:
-                print(f"{datetime.now()}:    No backends available. Exiting.")             
+                print(f"{datetime.now()}:    No Backend available. Exiting.")             
                 retryAfter = str(self._get_soonest_retry_after())                
                 return Response(429, content='', headers={'Retry-After': retryAfter})
             
             # Update URL and host header
-            request.url = request.url.copy_with(host=self.Backends[backendIndex].host)                
+            request.url = request.url.copy_with(host=self.backends[backendIndex].host)                
             headers = request.headers.copy()    # Create a mutable copy of the headers
-            headers['host'] = self.Backends[backendIndex].host        
+            headers['host'] = self.backends[backendIndex].host        
             request.headers = headers           # Assign the modified headers back to request.headers
 
             # Send the request to the backend
@@ -157,7 +161,7 @@ class LoadBalancer(BaseTransport):
             if response is not None and (response.status_code == 429 or response.status_code >= 500):                
                 # If the server is throttling or there's a server error, retry with a different server
                 print(f"{datetime.now()}:   Request sent to server: {request.url}, Status Code: {response.status_code} - FAIL")
-                self.statistics.update_backend_stats(self.Backends[backendIndex].host, False)  # Request failed
+                self.statistics.update_backend_stats(self.backends[backendIndex].host, False)  # Request failed
                 
                 currentBackendIndex = backendIndex
 
@@ -169,30 +173,28 @@ class LoadBalancer(BaseTransport):
                 if retryAfter == -1:
                     retryAfter = int(response.headers.get('x-ratelimit-reset-requests', '10'))
 
-                print(f"{datetime.now()}:   Backend {self.Backends[currentBackendIndex].host} is throttling. Retry after {retryAfter} second(s).")
+                print(f"{datetime.now()}:   Backend {self.backends[currentBackendIndex].host} is throttling. Retry after {retryAfter} second(s).")
 
-                backend = self.Backends[currentBackendIndex]
+                backend = self.backends[currentBackendIndex]
                 backend.is_throttling = True
                 backend.retry_after = datetime.now(tzutc()) + timedelta(seconds=retryAfter)
-                self._get_remainingBackends()            
+                self._get_remainingBackend()            
                 continue
 
             elif response is not None and (response.status_code >= 200 and response.status_code <= 399):
                 # Successful requests
                 print(f"{datetime.now()}:   Request sent to server: {request.url}, Status code: {response.status_code}")
-                self.Backends[backendIndex].successful_call_count += 1
-                self.statistics.update_backend_stats(self.Backends[backendIndex].host, True)  # Request was successful
+                self.backends[backendIndex].successful_call_count += 1
+                self.statistics.update_backend_stats(self.backends[backendIndex].host, True)  # Request was successful
                 break
 
             else:
                 # Would likely be a 4xx error other than 429
                 print(f"{datetime.now()}:   Request sent to server: {request.url}, Status code: {response.status_code} - FAIL")
-                self.statistics.update_backend_stats(self.Backends[backendIndex].host, False)  # Request failed
+                self.statistics.update_backend_stats(self.backends[backendIndex].host, False)  # Request failed
                 break
 
-        if self._remainingBackends == 0:
-            print(f"{datetime.now()}:   No backends available. Exiting.")
-            retryAfter = str(self._get_soonest_retry_after())
-            return Response(429, content='', headers={'Retry-After': retryAfter})
+        if self._remainingBackend == 0:
+            return self._return_429()
 
         return response
