37ec55d0c9f2da9be28478a584e4f015d844da3a
Simon Kurtz <simonkurtz@microsoft.com>
2024-05-13 22:26:01 -0400

    Create package src directory and move files

--- aoai.py
+++ aoai.py
@@ -1,7 +1,7 @@
 from azure.identity import DefaultAzureCredential
 from datetime import datetime
 from openai import AzureOpenAI, AsyncAzureOpenAI, DefaultHttpxClient, DefaultAsyncHttpxClient, NotFoundError
-from load_balancer import AsyncLoadBalancer, LoadBalancer, Backend
+from src.openai_loadbalancer_simon_kurtz.openai_load_balancer import AsyncLoadBalancer, LoadBalancer, Backend
 from typing import List
 import asyncio
 import time
