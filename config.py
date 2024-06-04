995179858dfc490b25bd422e847449d191c7db49
Simon Kurtz <simonkurtz@microsoft.com>
2024-06-04 14:01:22 -0400

    Fix config

--- config.py
+++ config.py
@@ -5,19 +5,18 @@
 from typing import List
 from src.openai_priority_loadbalancer.openai_priority_loadbalancer import Backend
 
-class Config:
-    NUM_OF_REQUESTS = 5
-    MODEL           = "<your-aoai-model>"  # the model, also known as the Deployment in Azure OpenAI, is common across standard and load-balanced requests
-    AZURE_ENDPOINT  = "https://oai-eastus-xxxxxxxx.openai.azure.com"
+NUM_OF_REQUESTS = 5
+MODEL           = "<your-aoai-model>"  # the model, also known as the Deployment in Azure OpenAI, is common across standard and load-balanced requests
+AZURE_ENDPOINT  = "https://oai-eastus-xxxxxxxx.openai.azure.com"
 
-    backends: List[Backend] = [
-        Backend("oai-eastus-xxxxxxxx.openai.azure.com", 1),
-        Backend("oai-southcentralus-xxxxxxxx.openai.azure.com", 1),
-        Backend("oai-westus-xxxxxxxx.openai.azure.com", 1)
-    ]
+backends: List[Backend] = [
+    Backend("oai-eastus-xxxxxxxx.openai.azure.com", 1),
+    Backend("oai-southcentralus-xxxxxxxx.openai.azure.com", 1),
+    Backend("oai-westus-xxxxxxxx.openai.azure.com", 1)
+]
 
-    backends_with_api_keys: List[Backend] = [
-        Backend("oai-eastus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
-        Backend("oai-southcentralus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
-        Backend("oai-westus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
-    ]
+backends_with_api_keys: List[Backend] = [
+    Backend("oai-eastus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+    Backend("oai-southcentralus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'),
+    Backend("oai-westus-xxxxxxxx.openai.azure.com", 1, None, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+]
